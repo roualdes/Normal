d2c70e151182144bf86f47fdfd6ce0df395546b9
Edward A. Roualdes <eroualdes@csuchico.edu>
2021-03-23 13:49:30 -0700

    remove secret from url

--- src/app.jl
+++ src/app.jl
@@ -1,5 +1,9 @@
 using Pluto
 
-Pluto.run(notebook = "src/normal.jl",
+# TODO create a copy with a uuid of src/normal.jl and load it
+
+Pluto.run(notebook = "src/normal.jl", require_secret_for_access = false,
           port = parse(Int, ARGS[1]), host = "0.0.0.0",
           show_file_system = false, launch_browser = false)
+
+# TODO figure out how to delete all but src/normal.jl and src/app.jl when webpage closed
