80f13a23873cb9761d3352ef262b0d3d41eed0ce
Edward A. Roualdes <eroualdes@csuchico.edu>
2021-03-23 12:54:45 -0700

    try again: remove Normal package, add a few options to Pluto

--- src/app.jl
+++ src/app.jl
@@ -1,3 +1,4 @@
 using Pluto
 
-Pluto.run(notebook = "normal.jl", port = parse(Int, ARGS[1]))
+Pluto.run(notebook = "normal.jl", port = parse(Int, ARGS[1]),
+          show_file_system = false, launch_browser = false)
